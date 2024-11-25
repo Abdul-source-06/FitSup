88c28656dc06780fa2575b6bca3425f06d50f943
Abdul <ahschohan06@gmail.com>
2024-11-25 16:05:58 +0100

    componente login creado
    he quitado todos los componentes para empezar la aplicacion desde zero, y he creado nuevo componente llamado login

--- FitSup/src/App.vue
+++ FitSup/src/App.vue
@@ -1,26 +1,23 @@
-<script setup>
-
-</script>
-
+<!-- src/App.vue -->
 <template>
-  <header class="flex flex-col items-center gap-6 lg:flex-row lg:items-start lg:justify-between p-6 bg-gray-100">
-    <div class="text-center lg:text-left">
-      <HelloWorld msg="You did it!" />
-
-      <nav class="mt-4 space-x-4">
-        <RouterLink to="/" class="px-4 py-2 border rounded-lg text-gray-700 hover:bg-gray-200">
-          Home
-        </RouterLink>
-        <RouterLink to="/about" class="px-4 py-2 border rounded-lg text-gray-700 hover:bg-gray-200">
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <!-- Llamar al componente Login -->
+    <Login />
+  </div>
 </template>
 
+<script>
+// Importa el componente Login
+import Login from './components/Login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Login // Registra el componente Login
+  }
+}
+</script>
+
 <style scoped>
-/* Puedes eliminar los estilos manuales que reemplazamos con Tailwind */
+/* Agregar estilo adicional si es necesario */
 </style>
