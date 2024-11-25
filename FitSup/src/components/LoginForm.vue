f458979fbdb152dfe8e2f5d0090ba04cf4d67676
Abdul <ahschohan06@gmail.com>
2024-11-25 18:24:50 +0100

    Login cambiado

--- FitSup/src/components/LoginForm.vue
+++ FitSup/src/components/LoginForm.vue
@@ -1,13 +1,20 @@
 <template>
-  <form @submit.prevent="handleLogin" class="space-y-4">
-    <label for="username" class="block mb-2">Username</label>
-    <input type="text" id="username" v-model="username" class="w-full p-2 mb-4 border border-gray-300 rounded-md text-black" />
+  <div class="flex items-center justify-center min-h-screen">
+    <form @submit.prevent="handleLogin" class="space-y-4">
+      <label for="username" class="block mb-2 flex justify-center">Username</label>
+      <input type="text" id="username" v-model="username"
+        class="w-full p-2 mb-4 border border-gray-300 rounded-md text-black text-center" />
 
-    <label for="password" class="block mb-2">Password</label>
-    <input type="password" id="password" v-model="password" class="w-full p-2 mb-4 border border-gray-300 rounded-md text-black" />
+      <label for="password" class="block mb-2 flex justify-center">Password</label>
+      <input type="password" id="password" v-model="password"
+        class="w-full p-2 mb-4 border border-gray-300 rounded-md text-black text-center" />
 
-    <button type="submit" class="w-full p-2 bg-blue-500 text-white rounded-md">Login</button>
-  </form>
+      <button
+        class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full">
+        Login
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
--- FitSup/src/views/Login.vue
+++ FitSup/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-orange-500 p-6 text-black" :style="{ backgroundColor: '#C02121' }">
-    <h2 class="text-2xl font-bold text-white">Login</h2>
+  <div class="p-6 text-black" :style="{ backgroundColor: '#C02121' }">
+    <h2 class="text-2xl font-bold text-white flex justify-center">Login</h2>
     <LoginForm />
   </div>
 
